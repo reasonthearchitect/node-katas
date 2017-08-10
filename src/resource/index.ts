2e12053ce037ca2c898587da234b6c693b7fe7ec
reasonthearchitect <rhys.personal@gmail.com>
2017-08-10 13:25:06 -0400

    Added the resource implementation and tests

--- src/resource/index.ts
+++ src/resource/index.ts
@@ -1,1 +1,2 @@
 export * from './iperson.by.email.resource';
+export * from './impl/person.by.email.resource';
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-var http = require('http');
+const http = require('http');
 
-var server = http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
