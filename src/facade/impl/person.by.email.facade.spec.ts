e0180697fc3df4fec5814eb4e9e00283ce93dc62
reasonthearchitect <rhys.personal@gmail.com>
2017-08-10 14:02:42 -0400

    Added the ready made facades to prepare for the graphql lesson

--- src/facade/impl/person.by.email.facade.spec.ts
+++ src/facade/impl/person.by.email.facade.spec.ts
@@ -1,3 +1,19 @@
 import 'reflect-metadata';
 import test from 'ava';
+import { PersonByEmailFacade } from '../../facade';
+import { IPersonByEmailResource } from '../../resource';
+import { Person } from '../../model';
 import * as TypeMoq from 'typemoq';
+
+test('Should save as input but return entity', async t => {
+    let mockPersonByEmailResource = TypeMoq.Mock.ofType<IPersonByEmailResource>();
+    mockPersonByEmailResource.setup(x => x.findByEmail(TypeMoq.It.isAny()))
+        .returns(() => {
+            return <any>Promise.resolve({
+                id: 'id001'
+            });
+        });
+    let facade = new PersonByEmailFacade(mockPersonByEmailResource.object);
+    const result = await <any>Promise.resolve(facade.findByEmail("email@email.com"));
+    t.is(result.id, 'id001');
+});
--- src/facade/index.ts
+++ src/facade/index.ts
@@ -1,1 +1,2 @@
 export * from './iperson.by.email.facade';
+export * from './impl/person.by.email.facade';
